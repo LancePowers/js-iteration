922e64ed67840df772b2f9cc53d53a7ea89eaf11
Lance <Lance@thelpstory.com>
2015-07-19 15:33:25 -0600

    Cloned file and created some code in dom.js to get the select element. Currently only able to log the value of the first of the selected items

--- js/dom.js
+++ js/dom.js
@@ -1,11 +1,20 @@
-/*
 
-Hints for the first problem:
 
-  1. Find the button with the ID of `sum-all`
-  2. Add an event listener that:
-    - finds the select element by name (use getElementsByName)
-    - passes all of its options to the `sum` function defined in `logic.js`
-    - takes the value returned by the `sum` function and displays it on the page
+// Hints for the first problem:
+//
+//   1. Find the button with the ID of `sum-all`
+var sumAll = document.getElementById('sum-all');
+console.log(sumAll);
 
-*/
+//   2. Add an event listener that:
+//     - finds the select element by name (use getElementsByName)
+var select = document.getElementsByName('prices');
+function test(){
+console.log(select[0][0].value);
+
+}
+sumAll.onclick = test;
+//     - passes all of its options to the `sum` function defined in `logic.js`
+//     - takes the value returned by the `sum` function and displays it on the page
+//
+// */
